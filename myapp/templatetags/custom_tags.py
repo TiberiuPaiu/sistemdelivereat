cfe87867026152e090ffb7263f087eb40fb25739
TiberiuPaiu <tiberiuip97@gmail.com>
2024-05-12 19:54:40 +0200

    Moastrar el numero de usuarios el numero de platos y el numero reseñas del  restaurante

--- myapp/templatetags/custom_tags.py
+++ myapp/templatetags/custom_tags.py
@@ -1,9 +1,23 @@
 from django import template
 
-
+from myapp.models import Plato, Restaurante, Repartidor, Cocina, ResenaRestaurante
 from sistemdelivereat.utils.carito_copras import CarritoDeCompras
 
 register = template.Library()
 
+@register.simple_tag
+def platos_count_ddbb(restaurante_id):
+    return Plato.objects.filter(restaurante_id=restaurante_id).count()
+
+@register.simple_tag
+def user_count_ddbb(restaurante_id):
+     num_cocina = Cocina.objects.filter(restaurante_id=restaurante_id).count()
+     num_repartidor = Repartidor.objects.filter(restaurante_id=restaurante_id).count()
+     return num_cocina + num_repartidor
+
+
+@register.simple_tag
+def resena_count_ddbb(restaurante_id):
+     return ResenaRestaurante.objects.filter(restaurante_id=restaurante_id).count()
 
 
