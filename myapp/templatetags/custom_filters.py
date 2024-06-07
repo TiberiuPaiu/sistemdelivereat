f86c20c721fad01c1e5d0e8791d5a3a7cddba11a
TiberiuPaiu <tiberiuip97@gmail.com>
2024-06-07 19:17:31 +0200

    Detalles del pedido en formato factura

--- myapp/templatetags/custom_filters.py
+++ myapp/templatetags/custom_filters.py
@@ -11,3 +11,19 @@ def custom_get(dictionary, key):
 def descuento(plato):
     precio_con_descuento = plato.precio * (1 - Decimal(str(plato.descuento)) / 100)
     return precio_con_descuento.quantize(Decimal('0.00'))
+
+@register.filter(name='tax_plat')
+def tax_plat(precio):
+
+    precio_con_tax = ((precio * 10 ) / 100 )
+    return precio_con_tax.quantize(Decimal('0.00'))
+
+
+@register.filter(name='tax_trans')
+def tax_trans(precio):
+    precio_con_tax = (((precio * 3) / 100))
+    return precio_con_tax.quantize(Decimal('0.00') + Decimal('0.50'))
+
+@register.filter(name='tax_full')
+def tax_full(precio):
+    return precio.quantize(Decimal('0.00')) + tax_plat(precio) + tax_trans(precio)
